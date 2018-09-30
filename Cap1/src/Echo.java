e167b8f94483a6f67c0ad4809ade110e68a39911
Lumi <Lumi@DESKTOP-4PJKITJ>
2018-09-30 11:32:04 +0300

    initial

---
+++ Cap1/src/Echo.java
@@ -0,0 +1,8 @@
+
+public class Echo {
+int count = 0;
+
+public void hello() {
+	System.out.println("Hello.....");
+}
+}
---
+++ Cap1/src/EchoTestDrive.java
@@ -0,0 +1,20 @@
+
+public class EchoTestDrive {
+
+	public static void main(String[] args) {
+		
+		Echo e1=new Echo();
+		int x=0;
+		Echo e2=new Echo();
+		while (x<4) {
+			e1.hello();
+			e1.count=e1.count+1;
+			
+			e2.count=e2.count+1;
+			e2.count=e2.count+e1.count;
+			x=x+1;
+		}
+System.out.println(e2.count);
+	}
+
+}
